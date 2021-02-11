05deab25c2792da001675176fe39a31c3290ff9b
ʄɽɑɳƙꍟɳʂʈꍟꀤɳ™ ❬❬kiռց ૨ip⛦❭❭ <64843897+DONOFTELEGRAM@users.noreply.github.com>
2021-02-11 20:57:05 +0530

    Update and rename CamScanner_🖨️.py to CamScanner_.py

--- DaisyX/modules/CamScanner_.py
+++ DaisyX/modules/CamScanner_.py
@@ -10,4 +10,4 @@ You can simply draw a border(a black square) around the portion you want to scan
 If you are still messed up send `/helpcamscanner` in pm for the tutorial !
 """
 
-__mod_name__ = "CamScanner 🖨️"
+__mod_name__ = "CamScanner"
